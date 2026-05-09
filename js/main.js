16205bfbe828f5ef55fcfb0abc8c7e7fe5c47ca0
Rika Nurhalisa <rikanurhalisa@gmail.com>
2026-05-09 11:49:52 +0700

    Update teks

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
-
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('I LOVE U SAYANG').split('');
     const titleElement = document.getElementById('title');
+    const subtitleElement = document.getElementById('subtitle');
+    
     let index = 0;
+    let subIndex = 0;
 
     function appendTitle() {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 300); 
+      } else {
+        // Setelah title selesai, mulai ketik subtitle
+        appendSubtitle();
+      }
+    }
+
+    function appendSubtitle() {
+      if (subIndex < subtitleText.length) {
+        subtitleElement.innerHTML += subtitleText[subIndex];
+        subIndex++;
+        setTimeout(appendSubtitle, 100); // Nama diketik lebih cepat
       }
     }
 
     appendTitle();
-
     clearTimeout(c);
   }, 1000);
 };
